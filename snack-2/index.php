527eb139873d526719f3eea9cae38602cd182587
marcobusacca <marcobusacca01@gmail.com>
2023-07-03 13:12:15 +0200

    first commit

---
+++ snack-2/index.php
@@ -0,0 +1,19 @@
+<!-- SCRIPT PHP -->
+<?php 
+
+?>
+
+<!-- TEMPLATE HTML -->
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CDN CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!-- Head Title -->
+        <title>php-snack-2</title>
+    </head>
+    <body>
+    </body>
+</html>
